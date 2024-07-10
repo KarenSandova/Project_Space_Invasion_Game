a8f740f74be49a4e1d6da709059f8481445f021d
KarenSandova <sandovalk061@gmail.com>
2024-07-10 12:07:17 -0500

    Add files via upload

--- PROJECT FIN KJ/hola.py
+++ PROJECT FIN KJ/hola.py
@@ -1,1 +1,20 @@
-print (hola)
+import pygame
+import random
+
+pygame.init()
+pygame.mixer.init()
+
+fondo = pygame.image.load('iamgenes/fondo.png')
+laser_sonido = pygame.mixer.Sound ('laser.wav')
+explosion_sonido = pygame.mixer.Sound('explosion.wav')
+golpe_sonido = pygame.mixer.Sound('golpe.wav')
+
+explosion_list = []
+for i in range (1,13):
+    explosion = pygame.image.load(//EXPLISION )     
+    explosion_list.append(explosion)
+    
+width = 
+height=
+window = 
+pygame.display.set_caption()
